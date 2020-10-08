db57c4af5aa44d16e285173c0ee302a4bc6ea061
Vartan Benohanian <vartanbeno@gmail.com>
2020-10-07 21:18:46 -0400

    Add options to get temperature in different units
    Signed-off-by: Vartan Benohanian <vartanbeno@gmail.com>

--- pkg/openweather/openweather-options_test.go
+++ pkg/openweather/openweather-options_test.go
@@ -19,19 +19,46 @@ func TestWithHTTPClient(t *testing.T) {
 }
 
 func TestWithUserAgent(t *testing.T) {
-	c, err := New("", WithUserAgent("test"))
+	client, err := New("", WithUserAgent("test"))
 	require.NoError(t, err)
-	require.Equal(t, "test", c.UserAgent)
+	require.Equal(t, "test", client.UserAgent)
 }
 
 func TestWithBaseURL(t *testing.T) {
-	c, err := New("", WithBaseURL(":"))
+	client, err := New("", WithBaseURL(":"))
+	require.Error(t, err)
 	urlErr, ok := err.(*url.Error)
 	require.True(t, ok)
 	require.Equal(t, "parse", urlErr.Op)
 
 	baseURL := "http://localhost:8080"
-	c, err = New("", WithBaseURL(baseURL))
+	client, err = New("", WithBaseURL(baseURL))
 	require.NoError(t, err)
-	require.Equal(t, baseURL, c.BaseURL.String())
+	require.Equal(t, baseURL, client.BaseURL.String())
+}
+
+func TestUseUnits(t *testing.T) {
+	client, err := New("")
+	require.NoError(t, err)
+	require.Empty(t, client.units)
+
+	req, err := client.NewRequest(http.MethodGet, "api/v1/test", nil)
+	require.NoError(t, err)
+	require.Empty(t, req.URL.Query().Get("units"))
+
+	client, err = New("", UseMetricUnits)
+	require.NoError(t, err)
+	require.Equal(t, "metric", client.units)
+
+	req, err = client.NewRequest(http.MethodGet, "api/v1/test", nil)
+	require.NoError(t, err)
+	require.Equal(t, "metric", req.URL.Query().Get("units"))
+
+	client, err = New("", UseImperialUnits)
+	require.NoError(t, err)
+	require.Equal(t, "imperial", client.units)
+
+	req, err = client.NewRequest(http.MethodGet, "api/v1/test", nil)
+	require.NoError(t, err)
+	require.Equal(t, "imperial", req.URL.Query().Get("units"))
 }
